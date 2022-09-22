e22ddd6552f119f4f3fa38dadaf982bdacaa7ba6
Gulnaz Gimaletdinova <gulnaz1024@yandex.ru>
2022-09-22 17:18:40 +0600

    fix bug of login (table update)

--- src/company/Main.java
+++ src/company/Main.java
@@ -20,14 +20,17 @@ public class Main {
         Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         Statement statement = connection.createStatement();
         PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NEW);
-        String name = "";
-        String surname = "";
+        String name = "",
+                surname = "",
+                password = "";
+        int login = 0;
+
         boolean wrongInput = false;
 
         try (Scanner scanner = new Scanner(System.in)) {
             do {
                 if (wrongInput) {
-                    System.out.println("Пароль или Логин введены не верно! ");
+                    System.out.println("Пароль или Логин заданы не верно! ");
                 }
 
                 System.out.print("Введите имя: ");
@@ -35,25 +38,32 @@ public class Main {
 
                 System.out.print("Введите фамилию: ");
                 surname = scanner.nextLine();
+
+                System.out.print("Придумайте пароль: ");
+                password = scanner.nextLine();
+
                 wrongInput = true;
             } while (name.trim().isEmpty() || surname.trim().isEmpty());
 
-
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, surname);
-            preparedStatement.setInt(3, 1);
-            preparedStatement.setString(4, "dgfg");
+            preparedStatement.setInt(3, login);
+            preparedStatement.setString(4, password);
             preparedStatement.execute();
 
+            String SELECT_id = "SELECT id FROM employee WHERE name = \'" + name + "\' AND surname = \'" + surname + "\'";
+            ResultSet result = statement.executeQuery(SELECT_id);
+            while (result.next()) {
+                login = result.getInt(1);
+                System.out.println("Регистрация прошла успешно! вот ваш логин: " + result.getInt(1));
+            }
+//            preparedStatement.executeUpdate();
+            String UPDATE_LOGIN = "UPDATE employee SET login = \'" + login + "\' WHERE login = 0";
+            statement.executeUpdate(UPDATE_LOGIN);
 
-        }
 
-        String SELECT_id = "SELECT id FROM employee WHERE name = \'" + name + "\'";
-        ResultSet result = statement.executeQuery(SELECT_id);
-        while (result.next()) {
-            System.out.println("Регистрация прошла успешно! вот ваш логин: " + result.getInt(1));
-        }
 //        Employee a = new Employee("dim", "rr", 1, "password");
 //        a.printInfo();
+        }
     }
 }
